ba44a4f1d080384723e6b23a1e3856986b3b08a2
Alex <alexerror95@gmail.com>
2019-05-26 14:41:33 +0300

    'front'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RestService} from './services/rest.services';
+import {Urls} from './models/urls.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SanyaSong';
-}
+export class AppComponent {}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RestService } from './services/rest.services';
+import {RouterModule} from '@angular/router';
+import {appRoutes} from './routers.register';
+import {HttpModule} from '@angular/http';
+import {CommonService} from './services/common.service';
+import {MaterialModule} from './modules/material/material.module';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    CommonModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+    )
   ],
-  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    RestService,
+    CommonService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
